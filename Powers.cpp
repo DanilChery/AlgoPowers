f77f1f5ba14b0233619782cbb6b6c304af9683ec
DanilChery <52321592+DanilChery@users.noreply.github.com>
2020-11-27 19:44:56 +0300

    update

--- Powers.cpp
+++ Powers.cpp
@@ -1,15 +1,33 @@
-﻿#include <iostream>
+#include <iostream>
 
 using namespace std;
+
+double func(double a, double b, double n, double N) {
+	if (2 * n < N) {
+		return func(a, b * b, n * 2, N);
+	}
+	else {
+		for (int i = 0; i < N-n; i++)
+		{
+			b *= a;
+		}
+		return b;
+	}
+}
 int main()
 {
-	int a, b;
+	double a, b;
 	cin >> a >> b;
-	int res = 1;
+	double res = 1;
+// 1. Через обычные итерации.
 	for (int i = 0; i < b; i++)
 	{
 		res *= a;
 	}
-	cout << res;
+	cout << res << endl;
+// 2. Через степень двойки с домножением.
+	res = func(a, a, 1, b);
+	cout << res << endl;
+
 }
 
